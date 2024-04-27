ed66a508027fbd3e3f58b7e999c1beb070e93f12
Almasyaqoob <167691572+Almasyaqoob@users.noreply.github.com>
2024-04-28 00:58:53 +0500

    Add files via upload

---
+++ 25 Alien_color_1.ts
@@ -0,0 +1,20 @@
+// Alien color #1: Imagian an alien was just shot down in a game, Create a variable 
+//called alien_color and assignit a value of 'green'.'yallow',or'red'.
+
+// write an if statement to test whether the alien's color is green. if print a
+// message that the player just earned 5 points.
+
+//Create a variabl called  alien_color
+
+let alien_color :string = 'green';
+if(alien_color ==='green'){
+    console.log("player just earned 5 points !");
+}
+
+// write one version of this program that passes the if test and another
+//fails.
+ alien_color  = 'yallow';
+ //(The version that fails will have no output.)
+if(alien_color ==='green'){
+    console.log("player just earned 5 points !");
+}
