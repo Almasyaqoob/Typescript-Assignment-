4e105c16245d887746dfde750ab5879b4007ae3c
Almasyaqoob <167691572+Almasyaqoob@users.noreply.github.com>
2024-04-28 00:48:53 +0500

    Add files via upload

---
+++ 12 Names.ts
@@ -0,0 +1,16 @@
+
+
+let names :string[] = ['Almas','eshal','aqsa','Heena'] ;
+
+const message :string ='welcome to typescript class: '
+
+
+for(let i=0; i<names.length; i++){
+
+
+    console.log(message + names[i]) ;
+
+
+}
+
+
