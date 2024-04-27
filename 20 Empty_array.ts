9cd33f146b5f979e3dc4a28e3a0fe3a3ae0032f9
Almasyaqoob <167691572+Almasyaqoob@users.noreply.github.com>
2024-04-28 00:55:44 +0500

    Add files via upload

---
+++ 20 Empty_array.ts
@@ -0,0 +1,12 @@
+
+//Think of something you could store in a array. 
+let items :string[] =  ['pakistan', 'karachi', 'urdu'];
+
+//Write a program  that creates a list containing these items.
+
+let items2 :string[] = [] ;
+items2.push('germany');
+items2.push('England');
+items2.push('india');
+
+console.log(items2);
