246d0ef5f742ce7026b334f4ba96bbfb82ec4f39
Almasyaqoob <167691572+Almasyaqoob@users.noreply.github.com>
2024-04-28 00:56:42 +0500

    Add files via upload

---
+++ 22 intentional_Error.ts
@@ -0,0 +1,13 @@
+// If you haven't received an array index error in one of your program yet, try to make one happen.
+
+// creating array
+let newArray :string[] = ['will smith',' jemes bond','tom cruise'];
+
+
+//Accessing array by index.
+console.log(newArray[5]);
+
+//make sure you correct the error before closing the program.
+
+// correct the error
+console.log(newArray);
