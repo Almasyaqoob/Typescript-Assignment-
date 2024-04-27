24a0b87990f31b19ccdb1c3ed8673e410ec79a86
Almasyaqoob <167691572+Almasyaqoob@users.noreply.github.com>
2024-04-28 01:01:06 +0500

    Add files via upload

---
+++ 29 favorite_fruit.ts
@@ -0,0 +1,17 @@
+let favorite_fruit: string[] = ["apple", "mango", "banana"];
+
+if(favorite_fruit.includes("apple")){
+    console.log("I really like apple");
+}
+if(favorite_fruit.includes("mango")){
+    console.log("I really like mango");
+}
+if(favorite_fruit.includes("banana")){
+    console.log("I really like banana");
+}
+if(favorite_fruit.includes("strawberry")){
+    console.log("I really like strawberries");
+}
+if(favorite_fruit.includes("orange")){
+    console.log("I really like orange");
+}
