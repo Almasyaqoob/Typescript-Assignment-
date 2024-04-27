53657a2c8847dd9511aaeab8aac2383bfa85e1c6
Almasyaqoob <167691572+Almasyaqoob@users.noreply.github.com>
2024-04-28 01:02:29 +0500

    Add files via upload

---
+++ 31 no_users.ts
@@ -0,0 +1,8 @@
+let userName: string[] =["admin" ,"junaid" , "zeeshan" , "kamran" , "daniyal"];
+if(userName.length === 0){
+    console.log ("we need to find some users!");
+}
+else{
+    userName =[];
+    console.log("All users Names have been removed  " + userName.length);
+}
