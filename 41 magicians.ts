57b0688c1aedc1ec6d0439208b120f93caf7cfcc
Almasyaqoob <167691572+Almasyaqoob@users.noreply.github.com>
2024-04-28 01:08:59 +0500

    Add files via upload

---
+++ 41 magicians.ts
@@ -0,0 +1,12 @@
+
+let magician: string[] = ["Harry potter", "Hernione Granger", "Ron weasley", "Albus Dumbledore "];
+
+function show_magicians(magicians: string[]){
+
+
+    magicians.forEach(element => {
+        console.log(element);
+        
+    });
+}
+show_magicians(magician)
