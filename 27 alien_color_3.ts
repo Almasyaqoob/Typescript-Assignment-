5f09747a8fdeb81c77b8dc58961be9ec5f503864
Almasyaqoob <167691572+Almasyaqoob@users.noreply.github.com>
2024-04-28 00:59:58 +0500

    Add files via upload

---
+++ 27 alien_color_3.ts
@@ -0,0 +1,61 @@
+
+
+
+let alienColors: string ="green";
+
+//version 1 of the program
+if(alienColors === "green"){
+    console.log("version 1  player earned 5 points")
+
+
+}else if(alienColors === "yallow"){
+    console.log("player earned 10 points");
+
+
+}else if(alienColors === "red"){
+    console.log("player earned 15 points");
+    
+}
+else{
+    console.log("please select right color")
+}
+//version 2 of the program
+
+alienColors = "yallow";
+
+
+if(alienColors === "green"){
+    console.log("player earned 5 points")
+
+
+}else if(alienColors === "yallow"){
+    console.log(" version 2 player earned 10 points");
+
+
+}else if(alienColors === "red"){
+    console.log("player earned 15 points");
+    
+}
+else{
+    console.log("please select right color")
+}
+//version 3 of the program
+alienColors = "red";
+
+
+if(alienColors === "green"){
+    console.log("player earned 5 points")
+
+
+}else if(alienColors === "yallow"){
+    console.log("player earned 10 points");
+
+
+}else if(alienColors === "red"){
+    console.log("version 3 player earned 15 points");
+    
+}
+else{
+    console.log("please select right color")
+}
+
