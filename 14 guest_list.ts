5305614962bce3a273584da29ece9d1f0502425c
Almasyaqoob <167691572+Almasyaqoob@users.noreply.github.com>
2024-04-28 00:50:05 +0500

    Add files via upload

---
+++ 14 guest_list.ts
@@ -0,0 +1,7 @@
+
+
+let guest_list :string[] = ['Eshal','anisa','aqsa'] ;
+
+for(let i=0; i<guest_list.length; i++){
+    console.log('Dear Ms,' + guest_list[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nThank You!')
+}
